327f06cf0471908bcf6b30dac0598e84c5d340b7
jws2170 <mpart2077@gmail.com>
2024-05-28 15:25:19 +0900

    [ADD] CameraWorldBorad

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] private float nomalSpeed;
-    [SerializeField] private float shiftSpeed;
+    [Header("CameraMoveInfo")]
+    [SerializeField] private float nomalSpeed = default;
+    [SerializeField] private float shiftSpeed = default;
+
+    [Header("WorldBord")]
+    [SerializeField] private Transform minPos = null;
+    [SerializeField] private Transform maxPos = null;
 
     private float speed = default;
     private Vector3 Dir = Vector3.zero;
 
     private void Update()
     {
+        PlayerInput();
+        CameraMove();
+    }
+
+    private void LateUpdate()
+    {
+        WorldBord();
+    }
+
+    /// <summary>
+    /// 플레이어의 입력을 받아 처리하는 함수
+    /// </summary>
+    private void PlayerInput()
+    {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
-        Dir = new Vector3 (x, 0f ,z);
+        Dir = new Vector3(x, 0f, z); //방향을 만듬
 
+        //쉬프트를 눌렀을때만 더 빠르게 움직이게 함
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speed = shiftSpeed;
@@ -25,8 +45,28 @@ public class CameraMovement : MonoBehaviour
         {
             speed = nomalSpeed;
         }
-            
+    }
 
-        transform.Translate(Dir * speed * Time.deltaTime);
+    /// <summary>
+    /// 방향을 받아서 카메라를 움직이는 함수
+    /// </summary>
+    private void CameraMove()
+    {
+        transform.Translate(Dir * speed * Time.deltaTime); //카메라 움직임
+    }
+
+    /// <summary>
+    /// 카메라 이동의 제한을 두는 월드보드를 만드는 함수
+    /// </summary>
+    private void WorldBord()
+    {
+        float posX = transform.position.x;
+        float posZ = transform.position.z;
+
+        //카메라의 트랜스폼값을 특정 범위 내에서만 움직이게함
+        posX = Mathf.Clamp(transform.position.x, minPos.position.x, maxPos.position.x); 
+        posZ = Mathf.Clamp(transform.position.z, minPos.position.z, maxPos.position.z); 
+
+        transform.position = new Vector3(posX, transform.position.y, posZ); 
     }
 }
--- Assets/Script/MouseDrag.cs
+++ Assets/Script/MouseDrag.cs
@@ -7,8 +7,8 @@ public class MouseDrag : MonoBehaviour
     [SerializeField] private RectTransform dragRectangle = null;  
 
     private Rect dragRect = default;
-    private Vector2 start = Vector2.zero;
-    private Vector2 end = Vector2.zero;
+    private Vector2 start = Vector2.zero; //사각형의 시작 위치
+    private Vector2 end = Vector2.zero; //사각형의 끝나는 위치
 
     private Camera mainCamera = null;
     private RTSUnitController rtsUnitController = null;
@@ -25,15 +25,15 @@ public class MouseDrag : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            start  = Input.mousePosition;
+            start  = Input.mousePosition; //처음 클릭했을때 위치
             dragRect = new Rect();
         }
 
         if (Input.GetMouseButton(0))
         {
-            end = Input.mousePosition;
+            end = Input.mousePosition; //드래그 중 위치
 
-            DrawDragRectangle();
+            DrawDragRectangle(); //드래그 범위 사각형을 그려줌
         }
 
         if (Input.GetMouseButtonUp(0))
@@ -43,7 +43,7 @@ public class MouseDrag : MonoBehaviour
             SelectUnits();
 
 
-            start = end = Vector2.zero;
+            start = end = Vector2.zero; //사각형 사라짐
             DrawDragRectangle();
         }
     }
@@ -58,18 +58,23 @@ public class MouseDrag : MonoBehaviour
         dragRectangle.sizeDelta = new Vector2(Mathf.Abs(start.x - end.x), Mathf.Abs(start.y - end.y)); //start와 end를 빼서 드래그한 범위를 구한다
     }
 
+    /// <summary>
+    /// 드래그의 범위를 랙트를 이용해 구하는 함수 
+    /// </summary>
     private void CalculateDragRect()
     {
-        if(Input.mousePosition.x < start.x)
+        
+        if (Input.mousePosition.x < start.x) //마우스를 아래서 위쪽으로 드래그 했을때
         {
             dragRect.xMin = Input.mousePosition.x;
             dragRect.xMax = start.x;
         }
-        else
+        else //마우스를 위에서 아래로 드래그 했을떄
         {
             dragRect.xMin = start.x;
             dragRect.xMax = Input.mousePosition.x;
         }
+
         if (Input.mousePosition.y < start.y)
         {
             dragRect.yMin = Input.mousePosition.y;
@@ -80,15 +85,19 @@ public class MouseDrag : MonoBehaviour
             dragRect.yMin = start.y;
             dragRect.yMax = Input.mousePosition.y;
         }
-    }
 
+
+    }
+    /// <summary>
+    /// 드래그한 영역안에 있는 유닛을 selectedUnitList에 넣어주는 함수
+    /// </summary>
     private void SelectUnits()
     {
-        foreach(UnitController unit in rtsUnitController.UnitList)
+        foreach(UnitController unit in rtsUnitController.UnitList) //UnitList에 요소가 있는 수만큼 만복
         {
-            if (dragRect.Contains(mainCamera.WorldToScreenPoint(unit.transform.position)))
+            if (dragRect.Contains(mainCamera.WorldToScreenPoint(unit.transform.position))) //랙트 범위 내에 유닛이 있다면 실행
             {
-                rtsUnitController.DragSelectUnit(unit);
+                rtsUnitController.DragSelectUnit(unit); //unit에 들어온 유닛 오브젝트를 selectedUnitList에 넣어줌
             }
         }
     }
