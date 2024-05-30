087a20eb405273cfea67c421b5a2de75182054a6
jws2170 <mpart2077@gmail.com>
2024-05-30 21:25:21 +0900

    [ADD] Unit Data

---
+++ Assets/Scriptable/UnitData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UnitData", menuName = "ScriptableObject/UnitData", order = int.MaxValue)]
+public class UnitData : ScriptableObject
+{
+    [SerializeField] private float damage = default;
+    public float Damage {  get { return damage; } }
+
+    [SerializeField] private float speed = default;
+    public float Speed { get { return damage; } }
+
+    [SerializeField] private float maxHP = default;
+    public float MaxHP { get { return damage; } }
+
+    [SerializeField] private float spawnSpeed = default;
+    public float SpawnSpeed { get { return damage; } }
+
+    [SerializeField] private float attackDistance = default;
+    public float AttackDistance { get { return damage; } }
+
+}
